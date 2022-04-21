79d48b235a966372ec55874d718813bba04b0a00
level237 <Bramslevel129@gmail.com>
2022-04-21 17:35:40 +0200

    Ready Input Data

---
+++ budgety/script.js
@@ -0,0 +1,62 @@
+// Budget Controller
+var budgetController=(function(){
+    // Some code
+})();
+
+
+
+// UI Controller
+var UIController=(function(){
+
+    var DOMstring ={
+        inputType:'.add__type',
+        inputDescription:'.add__description',
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
+    };
+        return {
+            getInput:function(){
+
+                return {
+                    type:document.querySelector(DOMstring.inputType).value,// will be either inc or exp
+                    description:document.querySelector(DOMstring.inputDescription).value,
+                    value:document.querySelector(DOMstring.inputValue).value,
+                }
+            },
+
+            getDOMstring:function(){
+                return DOMstring;
+            }
+        }
+
+})();
+
+
+// Global Controller
+var controller=(function(budgetCtrl,UICtrl){
+
+    var DOM=UICtrl.getDOMstring();
+    var ctrlAddItem=function(){
+         // 1.Get the  field input data
+        var input=UICtrl.getInput();
+        console.log(input);
+        //2. Add the item to the budget controller
+
+        // 3. add the item to the UI
+
+        //4. Calculate the budget
+
+        //5.display the budget on the UI
+        
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
+    document.addEventListener('keypress',function(event){
+        if(event.key==='Enter'){
+            
+            ctrlAddItem();
+            
+        }
+        
+    });
+})(budgetController,UIController); 
