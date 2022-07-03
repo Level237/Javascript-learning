0a85a97fd37782e1bd8365da0d4dc3ea2209e8d6
level237 <Bramslevel129@gmail.com>
2022-07-03 14:58:52 +0200

    coding chalenge 5:implementation

--- Js-Basics-Section2/script.js
+++ Js-Basics-Section2/script.js
@@ -508,3 +508,81 @@ for(var i=john.length -1;i>=0;i--){
 /************************************************
  * Coding Chalenge 5
  */
+
+var john = {
+    fullName:'John Swith',
+    'bills':[124,48,268,180,42],
+    calcTips:function(){
+        this.tips=[];
+        this.finalValues=[];
+
+        for(var i=0;i< this.bills.length;i++){
+            // Determine percentage based on tipping rules 
+            
+            var percentages;
+            var bill =this.bills[i];
+            if(bill<50){
+                percentages=.2;
+            }else if(bill>50 && this.bills[i]<200){
+                percentages=.15;
+            }else{
+                percentages=.1;
+            }
+
+            // Add results to the corresponding array
+            this.tips[i]=bill*percentages;
+            this.finalValues[i]=bill + bill * percentages;
+        }
+    }
+}
+
+
+var mark = {
+    fullName:'Mark',
+    'bills':[77,475,110,45],
+    calcTips:function(){
+        this.tips=[];
+        this.finalValues=[];
+
+        for(var i=0;i< this.bills.length;i++){
+            // Determine percentage based on tipping rules 
+            
+            var percentages;
+            var bill =this.bills[i];
+            if(bill<50){
+                percentages=.2;
+            }else if(bill>100 && bill<300){
+                percentages=.1;
+            }else{
+                percentages=.25;
+            }
+
+            // Add results to the corresponding array
+            this.tips[i]=bill*percentages;
+            this.finalValues[i]=bill + bill * percentages;
+        }
+    }
+}
+
+    function calcAverage(tips){
+        var sum =0;
+        for (var i=0;i<tips.length;i++){
+            sum=sum+tips[i];
+        }
+        return sum/tips.length;
+    }
+
+    // Do the calculations
+john.calcTips();
+mark.calcTips();
+console.log(john,mark);
+
+john.average=calcAverage(john.tips);
+mark.average=calcAverage(mark.tips);
+console.log(john,mark);
+
+if(john.average> mark.average){
+    console.log(john.fullName + '\'s familly pays higher tips,with an average of $'+john.average);
+}else if(mark.average>john.average){
+    console.log(mark.fullName + '\'s familly pays higher tips,with an average of $'+mark.average);
+}
