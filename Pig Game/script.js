104f5f11989d8cdba0397f184c63103c04a226d3
level237 <Bramslevel129@gmail.com>
2022-07-04 17:48:00 +0200

    Game rules

---
+++ Pig Game/script.js
@@ -0,0 +1,10 @@
+/*************************
+ * Game Rules:
+ * - The Game has 2 players,playing in rounds 
+ * - In each turn, a player rolls a dice as many times as he whishes .Each result get added to his round
+    score
+    -But if the player rolls a 1, all his round score gets lost.After that,it's the next player's turn
+    - The player can choose to 'Hold', which means that his round score gets added to his GLBAL score.After 
+    that it's the next player's turn
+    - The first player to reach 100 points on global score wins the Game
+ */
