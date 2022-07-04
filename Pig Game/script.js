88c07d593033f50ed09ea9e82c79ee8b2381175a
level237 <Bramslevel129@gmail.com>
2022-07-04 19:00:33 +0200

    Pig Game:implementation

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -25,3 +25,25 @@ dice=Math.floor(Math.random() *6) + 1;
 
 console.log(dice);
 
+document.querySelector('#current--' +activePlayer).textContent=dice; 
+//document.querySelector('#current--' +activePlayer).innerHTML= '<em>' + dice + '</em>';
+var x= document.querySelector('#score--0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display='none'; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
