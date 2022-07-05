77ca5fdefae4c9483822922311763b12f488d5a7
level237 <Bramslevel129@gmail.com>
2022-07-05 09:05:26 +0200

    Event and Event Handling Rolling the dice

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -32,6 +32,10 @@ activePlayer=0;
 
 document.querySelector('.dice').style.display='none'; 
 
+document.getElementById('score--0').textContent='0';
+document.getElementById('score--1').textContent='0';
+document.getElementById('current--0').textContent='0';
+document.getElementById('curr-ent--1').textContent='0';
 
 document.querySelector('.btn--roll').addEventListener('click',function(){
    // Do something here
