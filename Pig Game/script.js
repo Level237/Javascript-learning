587081b46f599b0e7719cdd501d7624ffbe60cb1
level237 <Bramslevel129@gmail.com>
2022-07-07 23:05:09 +0200

    Creating a Game InitialiZation Function

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -17,9 +17,7 @@
 
 var scores,roundScore,activePlayer,dice;
 
-scores=[0,0];
-roundScore=0;
-activePlayer=0;
+initGame();
 
 //dice=Math.floor(Math.random() *6) + 1;
 
@@ -30,12 +28,7 @@ activePlayer=0;
 //var x= document.querySelector('#score--0').textContent;
 //console.log(x);
 
-document.querySelector('.dice').style.display='none'; 
 
-document.getElementById('score--0').textContent='0';
-document.getElementById('score--1').textContent='0';
-document.getElementById('current--0').textContent='0';
-document.getElementById('current--1').textContent='0';
 
 document.querySelector('.btn--roll').addEventListener('click',function(){
    // Do something here
@@ -95,6 +88,27 @@ function nextPlayer(){
       //document.querySelector('.player--1').classList.add('player--active');
 
       document.querySelector('.dice').style.display='none';
+}
+
+document.querySelector('.btn--new').addEventListener('click',initGame)
+
+function initGame(){
+   scores=[0,0];
+   roundScore=0;
+   activePlayer=0;
+   document.querySelector('.dice').style.display='none'; 
+
+   document.getElementById('score--0').textContent='0';
+   document.getElementById('score--1').textContent='0';
+   document.getElementById('current--0').textContent='0';
+   document.getElementById('current--1').textContent='0';
+   document.getElementById('name--0').textContent='Player 1';
+   document.getElementById('name--1').textContent='Player 2';
+   document.querySelector('.player--0').classList.remove('player--winner');
+   document.querySelector('.player--1').classList.remove('player--winner');
+   document.querySelector('.player--1').classList.remove('player--active');
+   document.querySelector('.player--0').classList.add('player--active');
+
 }
 
 
