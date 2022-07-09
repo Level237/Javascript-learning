be136780d3a55bcb910b6cc3fcc9ebfba785be21
level237 <Bramslevel129@gmail.com>
2022-07-09 07:48:59 +0200

    Chalenge PigGame:rules

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -120,6 +120,21 @@ function initGame(){
 }
 
 
+/*****************************************************
+ * Your 3 Chalenges
+ * Change the game to follow these rules:
+ * 
+ * 1- A player looses his ENTIRE score when he rolls two 6 in a row. After that,it's the next player's
+ * turn.(Hint: Always save the previous dice roll in a separate variable)
+ * 2- Add an input field to the HTML where players can set the winning score,so that they can change the 
+ * predefined score of 100.(Hint: you can read value with the value property in Javascript.this is a 
+ * good oportunity to use google to figure this out :)
+ * 3. Add another dice to the game , so that there are two dices now.The player looses his current score
+ * when one of them is a 1. (Hint: you will need css to position the second dice ,so take a look at the css 
+ * code for the first one.)
+ */
+
+
 
 
 
