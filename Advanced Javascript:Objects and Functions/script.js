65662749280ba4bf035626ba350499820f678105
level237 <Bramslevel129@gmail.com>
2022-07-09 15:27:14 +0200

    Coding Chalenge 7:rules

--- Advanced Javascript:Objects and Functions/script.js
+++ Advanced Javascript:Objects and Functions/script.js
@@ -220,7 +220,7 @@ game();
 /***********
  * Lecture :Bind call and apply
  * 
- * *************/
+
 
 
 var john ={
@@ -275,6 +275,7 @@ function calculateAge(elt){
 }
 
 
+
 function isFullAge(limit,el){
     return el>=limit;
 }
@@ -283,3 +284,82 @@ var ages=arrayCalc(years,calculateAge);
 var fullJapan= arrayCalc(ages,isFullAge.bind(this,20));
 console.log(ages);
 console.log(fullJapan);
+ * *************/
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************************
+ * Coding Chalenge 7
+ * 
+ * 
+ * 
+ * 
+ * -----------Let's build a fun quizz game in the console!-----
+ * 
+ * 1. Build a function constructor called Question to describe a question. A question should include:
+ * a) Question itself
+ * b)the answers from which the player can choose the correct one (choose an adequate data structure here,
+ * array,object,etc..)
+ * 
+ * 2. Create a couple of questions using the constructor
+ * 
+ * 3.Store them all inside an array
+ * 
+ * 4.Select one random question and log it on the console,together with the possible answers (each
+ * questios should have a number) (Hint: write a method for the questions objects for this task).
+ * 
+ * 5.Use the 'prompt' function to ask the user for the correct answer.The user should input the number of
+ * the correct answer such as you displayed it on task 4.
+ * 
+ * 6. Check if the answer is correct and print to the console whether the correct ot nor (Hint:
+ * write another method for this).
+ * 
+ * 7. Suppose this code would be a plugin for other programmers to,use in their code.So make sure that all 
+ * your code is private and doesn't interface with the other programmers code( Hint: We learned a special 
+ * technique to do exactly that).
+ * 
+ * 
+ * ---------------Expert level-----------
+ * 
+ * 8- After you display the result,display the next random question,so that the game never ends (Hint:
+ * Write a function for this and call it right after displaying the result)
+ * 
+ * 
+ * 9- Be careful: After Task 8,the game literally never ends.So include the option to quid the game if the 
+ * user writes 'exit'  instead of the answer.In this case , Don't call the function from task 8.
+ * 
+ * 10. Track the user's score to make the game more fun! So each time an answer is correct ,add 1 point to
+ * the score (Hint: I'm going to use the power of closures for this,but you don't have to,just do this
+ * with the tools you feel more confortable at this point).
+ * 
+ * 11.Display the score in the console.Use  yet another method for this.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
